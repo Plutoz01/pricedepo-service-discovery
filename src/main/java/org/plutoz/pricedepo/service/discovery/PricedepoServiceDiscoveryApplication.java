6f1ba5867c8dd4a49940b5cd984a793f03c4da26
László Csatlós <plutoz01@gmail.com>
2017-01-13 19:32:28 +0100

    Initial Arch

---
+++ src/main/java/org/plutoz/pricedepo/service/discovery/PricedepoServiceDiscoveryApplication.java
@@ -0,0 +1,12 @@
+package org.plutoz.pricedepo.service.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricedepoServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricedepoServiceDiscoveryApplication.class, args);
+	}
+}
